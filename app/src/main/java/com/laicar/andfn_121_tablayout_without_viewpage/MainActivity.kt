f733e3ef72fc7a6e2b05246868a6e754a6b9877e
启辰 <akluse@qq.com>
2019-03-08 14:09:10 +0800

    project init

---
+++ app/src/main/java/com/laicar/andfn_121_tablayout_without_viewpage/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.laicar.andfn_121_tablayout_without_viewpage
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
